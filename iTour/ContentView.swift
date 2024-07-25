c714442ceee2c1eb4041cf0be6a9914785462a69
Stewart Chan <stew.chan@gmail.com>
2024-07-25 10:45:24 -0400

    Initial Commit

---
+++ iTour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTour
+//
+//  Created by Stewart Chan on 2024-07-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Stewart Chan on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
