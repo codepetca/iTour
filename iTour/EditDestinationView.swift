8476bb860931df31e5d411887d04315558d646a0
Stewart Chan <stew.chan@gmail.com>
2024-07-25 17:46:41 -0400

    Add Navigation, add new destination

---
+++ iTour/EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour
+//
+//  Created by Stewart Chan on 2024-07-25.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
